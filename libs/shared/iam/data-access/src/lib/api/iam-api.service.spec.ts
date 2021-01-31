b604dacd3a947f461d425a2e59319124ade7f396
Daniel Quintero <danielpiedraquintero@gmail.com>
2021-01-31 15:44:35 +0100

    feat: adds service unit tests

--- libs/shared/iam/data-access/src/lib/api/iam-api.service.spec.ts
+++ libs/shared/iam/data-access/src/lib/api/iam-api.service.spec.ts
@@ -1,19 +1,47 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  createHttpFactory,
+  HttpMethod,
+  SpectatorHttp,
+} from '@ngneat/spectator/jest';
+import { IamApiService as ServiceUnderTest } from './iam-api.service';
 
-import { IamApiService } from './iam-api.service';
+describe(ServiceUnderTest.name, () => {
+  let spectator: SpectatorHttp<ServiceUnderTest>;
+  let service: ServiceUnderTest;
 
-describe('IamApiService', () => {
-  let service: IamApiService;
+  const email = 'test@email.com';
+  const password = 'password';
+  const firstName = 'firstName';
+  const lastName = 'lastName';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-    });
-    service = TestBed.inject(IamApiService);
+  const createHttp = createHttpFactory({
+    service: ServiceUnderTest,
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    spectator = createHttp();
+    service = spectator.service;
+  });
+
+  afterEach(() => spectator.controller.verify());
+
+  it('signs in a user', () => {
+    const { expectOne, flushAll } = spectator;
+
+    service.signin({ email, password }).subscribe();
+
+    const testRequest = expectOne('/api/signin', HttpMethod.POST);
+    flushAll([testRequest], [{}]);
+  });
+  it('signs up a user', () => {
+    const { expectOne, flushAll } = spectator;
+
+    service.signup({ email, password, firstName, lastName }).subscribe();
+
+    const testRequest = expectOne(
+      'https://demo-api.now.sh/users',
+      HttpMethod.POST
+    );
+    flushAll([testRequest], [{}]);
   });
 });
--- libs/wcc/feature-iam/src/lib/iam.container.ts
+++ libs/wcc/feature-iam/src/lib/iam.container.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'fedex-iam',
   templateUrl: './iam.container.html',
   styleUrls: ['./iam.container.css'],
 })
-export class IamContainer implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class IamContainer {}
